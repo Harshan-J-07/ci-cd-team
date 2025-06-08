181b8c82268adda3496061ded0486e610a962a96
Harshan-J-07 <harshan1618x@gmail.com>
2025-06-08 23:19:54 +0530

    update port with default ip

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ app.get('/', (req, res) => {
     res.send("Jenkins build successfully complete");
 });
 
-const port = process.env.PORt || 3000;
-app.listen(port, () => console.log(`app listening on port ${port}`));
+// const port = process.env.PORt || 3000;
+// app.listen(port, () => console.log(`app listening on port ${port}`));
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
